4dfa98a4e1a7eb11a37448b850f3428be3239a8d
k-jaehyun <wogus146@gmail.com>
2023-12-12 15:46:51 +0900

    init : exclude = SecurityAutoConfiguration.class

--- src/main/java/com/sparta/plusweekreviewassignment/PlusWeekReviewAssignmentApplication.java
+++ src/main/java/com/sparta/plusweekreviewassignment/PlusWeekReviewAssignmentApplication.java
@@ -2,8 +2,9 @@ package com.sparta.plusweekreviewassignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PlusWeekReviewAssignmentApplication {
 
 	public static void main(String[] args) {
