0e42cc6e46cd8827a5d93193d2086ac860ad4dc0
k-jaehyun <wogus146@gmail.com>
2023-12-12 14:15:50 +0900

    init commit

---
+++ src/main/java/com/sparta/plusweekreviewassignment/PlusWeekReviewAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.plusweekreviewassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusWeekReviewAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusWeekReviewAssignmentApplication.class, args);
+	}
+
+}
