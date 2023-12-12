b6b5c442c90d707ad519aa52bab5286e43ab1f64
k-jaehyun <wogus146@gmail.com>
2023-12-12 16:00:05 +0900

    Feat : "중복된 닉네임입니다." 출력

---
+++ src/main/java/com/sparta/plusweekreviewassignment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.sparta.plusweekreviewassignment.exception;
+
+import com.sparta.plusweekreviewassignment.CommonResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<CommonResponseDto> IllegalArgumentExceptionHandler(IllegalArgumentException ex) {
+        CommonResponseDto commonResponseDto = new CommonResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(commonResponseDto,HttpStatus.BAD_REQUEST);
+    }
+}
